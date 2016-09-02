805af4621256115132ebb6cc9f8535850d623c36
Erik Cox <erikbcox@gmail.com>
2016-09-02 14:13:08 -0700

    Add anonymizeIp to analytics

--- js/analytics.js
+++ js/analytics.js
@@ -5,4 +5,5 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-83594912-1', 'auto');
 ga('set', 'checkProtocolTask', null);
++ga('set', 'anonymizeIp', true);
 ga('send', 'pageview', '/background.html');
